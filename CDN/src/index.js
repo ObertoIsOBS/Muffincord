d74f59d297c123a3ae166884e8ecc3d4f009df43
OBS <help.tgumbs1@gmail.com>
2021-03-28 22:13:06 -0400

    Add CDN

--- CDN/src/index.js
+++ CDN/src/index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const imgConversion = require("image-conversion");
+const fs = require("fs");
 
 const cdn = express();
 
 
 const got = require("got");
+const { fstat } = require("fs");
 
 cdn.get("/:f", async (req, res) => {
     const f = req.params.f;
@@ -14,30 +16,13 @@ cdn.get("/:f", async (req, res) => {
 
     const extension = f.split(".")[1];
 
-    const filecode = f.split(".")[0];
+    const filename = f.split(".")[0];
 
-    const serverRes = await got(`https://file.io/?search=${filecode}`, {headers: { Authorization: "Bearer RUBHXWG.WG1GPJ4-FF5MGNV-KWHD7V9-7SP94ZB"} });
+    const file = await fs.readFileSync(__dirname + "\\STORAGE\\" + filename + ".png");
 
-    if (!serverRes) return res.sendStatus(500);
-    
-    const serverResponse = serverRes.body;
+    if (!file) return res.sendStatus(404);
 
-    const imageStore = JSON.parse(serverResponse).files;
-
-    if (!imageStore) return res.sendStatus(404);
-
-    const fileInfo = imageStore[0];
-
-    if (!fileInfo) return res.sendStatus(404);
-
-    const filename = fileInfo.name.split(".")[0];
-
-    const fileExtension = fileInfo.name.split(".")[1];
-
-    const file = await got(`https://file.io/${fileInfo.key}`);
-
-    res.send(file);
-
-})
+    res.sendFile(__dirname + "\\STORAGE\\" + filename + ".png");
+});
 
 cdn.listen(70)
